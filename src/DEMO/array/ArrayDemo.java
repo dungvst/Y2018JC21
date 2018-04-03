af5161efb8a6996fb45be2a758d2cbd46d3dd565
dungvst <dungvst@gmail.com>
2018-04-03 21:19:43 +0700

    array demo

--- src/DEMO/array/ArrayDemo.java
+++ src/DEMO/array/ArrayDemo.java
@@ -1,27 +1,67 @@
 package DEMO.array;
 
+import java.util.Arrays;
+
 public class ArrayDemo {
 
     public static void main(String[] args) {
         // Khởi tạo  array
-        /*int []a = {1,2,4};
-        int []b = {1,2,4};
-        int []c = new int[10];
+        int []a = {1,2,4};
+        int []b = new int[a.length];
+        b[2] = 22;
 
-        // Copy array
-        System.arraycopy(a,1,c,0,2);
+        int []arrC = new int[10];
+        arrC[0] = 1;
+        arrC[2] = 2;
+        arrC[3] = 3;
+        arrC[1] = 4;
+
+       /* // Sử dụng fori
+        for (int i = 0; i < arrC.length; i++) {
+            System.out.println("Phan tu: "+ i + " value: "+ arrC[i]);
+        }
+
+        // Sử dụng foreach
+        for (int i: arrC) {
+            System.out.println("value: "+ arrC[i]);
+        }*/
+
+
+
+
+        /*// Copy array
+        System.arraycopy(a,0,b,0,2);
 
         // Loop throw array by using foreach
-        for (int x: c) {
+        for (int x: b) {
             System.out.println(x);
         }*/
 
         // Pass array to the method
-        int x = 1;
+       /* int x = 1;
         int []arr = new int[10];
         method(x, arr);
         System.out.println("x: "+ x);
-        System.out.println("arr[0]: "+ arr[0]);
+        System.out.println("arr[0]: "+ arr[0]);*/
+
+       // Sort array
+        /*String[] arr1 = {"abc", "b", "dd", "ad"};
+        System.out.println("Before sort");
+        for (String i : arr1) {
+            System.out.println(i);
+        }
+        Arrays.sort(arr1);
+
+        System.out.println("After sort");
+        for (String i : arr1) {
+            System.out.println(i);
+        }*/
+
+        int [][]arr2 = new int[10][10];
+        arr2[0][0] = 1;
+        arr2[0][1] = 2;
+
+
 
     }
 
