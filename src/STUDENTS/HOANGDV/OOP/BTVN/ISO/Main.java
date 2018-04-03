07b4983781fe9b1a9ab6432842e383ff9d232ba5
Do Hoang <dvhoang23@gmail.com>
2018-04-03 20:12:55 +0700

    BTNV ISO

--- src/STUDENTS/HOANGDV/OOP/BTVN/ISO/Main.java
+++ src/STUDENTS/HOANGDV/OOP/BTVN/ISO/Main.java
@@ -1,10 +1,30 @@
 package HOANGDV.OOP.BTVN.ISO;
 
-public class Main extends ChuanISO{
+public class Main extends ChuanISO {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		new Main().bangTan("CDMA");
+		System.out.println("Chọn tên các băng tần cần đăng ký");
+		System.out.println("1 - CDMA");
+		System.out.println("2 - WCDMA");
+		System.out.println("3 - GSM");
+		java.util.Scanner scan = new java.util.Scanner(System.in);
+		int select = scan.nextInt();
+		switch (select) {
+		case 1:
+			System.out.println("Tần số là: "+new Main().bangTan("CDMA"));
+			break;
+		case 2:
+			System.out.println("Tần số là: "+new Main().bangTan("WCDMA"));
+			break;
+		case 3:
+			System.out.println("Tần số là: "+new Main().bangTan("GSM"));
+			break;
+		default:
+			new Main().bangTan("No selection");
+			break;
+		}
+		scan.close();
 	}
 
 	@Override
@@ -13,13 +33,13 @@ public class Main extends ChuanISO{
 		String re = "";
 		switch (bt) {
 		case "CDMA":
-			re = "900 Mhz � 902 Mhz";
+			re = "900 Mhz � 902 Mhz";
 			break;
 		case "WCDMA":
-			re = " 903 Mhz � 905 Mhz";
+			re = " 903 Mhz � 905 Mhz";
 			break;
 		case "GSM":
-			re = "906 Mhz � 908 Mhz";
+			re = "906 Mhz � 908 Mhz";
 			break;
 		}
 		return re;
